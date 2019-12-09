3111ce2416ccd48db1d7dc3bbb9f837fb23b8ddb
Zhi Xiong <zhi.xiong@xtalpi.com>
2019-12-09 20:42:47 +0800

    sort

--- dev/src/views/Home.vue
+++ dev/src/views/Home.vue
@@ -16,12 +16,19 @@
       <vue-button class="aside-btns" size="mini" @click="toggleUpload">Toggle Upload</vue-button>
       <vue-button class="aside-btns" size="mini" @click="toggleDownload">Toggle Download</vue-button>
       <div class="aside-line"></div>
-      <vue-button class="aside-btns" size="mini" @click="reset">Reset</vue-button>
+      <vue-button class="aside-btns" size="mini" @click="changeColumnWidth">Change Column Width</vue-button>
     </aside>
     <section>
       <vuescroll :ops="scrollBarOpts" ref="vuescroll">
         <vue-table-dynamic 
           :params="params"
+          @select="onSelect"
+          @select-all="onSelectAll"
+          @selection-change="onSelectionChange"
+          @row-click="onRowClick"
+          @cell-click="onCellClick"
+          @upload="onUpload"
+          @download="onDownload"
         >
         </vue-table-dynamic>
       </vuescroll>
@@ -35,6 +42,8 @@ import NavMenu from './NavMenu.vue'
 import VueButton from './VueButton.vue'
 import VueMsg from 'vue-msgs'
 import vuescroll from 'vuescroll'
+import { saveAs } from 'file-saver'
+import { table, getBorderCharacters } from 'table'
 const cloneDeep = require('lodash.clonedeep')
 
 const random = (length) => {
@@ -45,9 +54,10 @@ const random = (length) => {
 
 const defaultTableParams = {
   data: [
-    [`Cell-${random()}`, `Cell-${random()}`, `Cell-${random()}`],
-    [`Cell-${random()}`, `Cell-${random()}`, `Cell-${random()}`],
-    [`Cell-${random()}`, `Cell-${random()}`, `Cell-${random()}`]
+    ['Index', `Data1`, `Data2`, `Data3`],
+    [1, `${random()}-Cell`, `${random()}-Cell`, `${random()}-Cell`],
+    [2, `${random()}-Cell`, `${random()}-Cell`, `${random()}-Cell`],
+    [3, `${random()}-Cell`, `${random()}-Cell`, `${random()}-Cell`]
   ],
   header: 'row',
   border: true,
@@ -55,7 +65,8 @@ const defaultTableParams = {
   enableUpload: true,
   enableDownload: true,
   enableSearch: true,
-  columnWidth: []
+  columnWidth: [{column: 0, width: 80}],
+  sort: [0, 1]
 }
 
 const tableHeaderTypes = ['', 'row', 'column']
@@ -68,7 +79,8 @@ export default {
       scrollBarOpts: {
         scrollPanel: { scrollingX: false },
         bar: { background: '#DFDFDF', opacity: 0.8 }
-      }
+      },
+      widthIncrement: 1
     }
   },
   methods: {
@@ -80,9 +92,9 @@ export default {
         return this.showMsg('warning', 'The number of rows cannot be more than 30.')
       }
 
-      let newRow = []
-      for (let i = 0; i < columnNum; i++) {
-        newRow.push(`Cell-${random()}`)
+      let newRow = [rowNum]
+      for (let i = 1; i < columnNum; i++) {
+        newRow.push(`${random()}-Cell`)
       }
 
       this.params.data.push(newRow)
@@ -104,8 +116,9 @@ export default {
         return this.showMsg('warning', 'The number of columns cannot be more than 12.')
       }
 
-      for (let i = 0; i < rowNum; i++) {
-        this.params.data[i].push(`Cell-${random()}`)
+      this.params.data[0].push(`Data${columnNum}`)
+      for (let i = 1; i < rowNum; i++) {
+        this.params.data[i].push(`${random()}-Cell`)
       }
     },
     deleteColumn () {
@@ -141,8 +154,54 @@ export default {
     toggleDownload () {
       this.params.enableDownload = !this.params.enableDownload
     },
+    changeColumnWidth () {
+      if (this.params.columnWidth[0].width >= 200) {
+        this.widthIncrement = -1
+      } else if (this.params.columnWidth[0].width <= 50) {
+        this.widthIncrement = 1
+      }
+      this.params.columnWidth[0].width += 10 * this.widthIncrement
+    },
     reset () {
       this.params = cloneDeep(defaultTableParams)
+    },
+    onSelect (checked, index, data) {
+      console.log('onSelect: ', checked, index, data)
+    },
+    onSelectAll (isCheckedAll) {
+      console.log('onSelectAll: ', isCheckedAll)
+    },
+    onSelectionChange (checkedDatas, checkedIndexs, checkedNum) {
+      console.log('onSelectionChange: ', checkedDatas, checkedIndexs, checkedNum)
+    },
+    onRowClick (index, data) {
+      console.log('onRowClick: ', index, data)
+    },
+    onCellClick (rowIndex, cellIndex, data) {
+      console.log('onCellClick: ', rowIndex, cellIndex, data)
+    },
+    onUpload (datas) {
+      console.log('onUpload: ', datas)
+      if (!(datas && datas.length > 0)) {
+        return this.showMsg('warning', 'The data is empty.')
+      }
+    },
+    onDownload (datas) {
+      if (!(datas && datas.length > 0)) {
+        return this.showMsg('warning', 'The data is empty.')
+      }
+
+      let output = table(datas, { 
+        border: getBorderCharacters(`void`),
+        columnDefault: {
+          paddingLeft: 0,
+          paddingRight: 2
+        },
+        drawHorizontalLine: () => { return false }
+      })
+
+      let blob = new Blob([output], {type: "application/octet-stream"})
+      saveAs(blob, "table")
     },
     // 消息弹框 type：success/info/warning/error
     showMsg (type, info) {
@@ -178,6 +237,8 @@ export default {
       display: block;
       width: 100%;
       margin-top: 12px;
+      font-size: 12px;
+      font-family: Arial;
     }
     .aside-line{
       display: block;
@@ -193,6 +254,11 @@ export default {
     left: 200px;
     right: 0;
     padding: 20px 25px;
+  }
+  .downloader{
+    height: 0;
+    width: 0;
+    visibility: hidden;
   }
 }
 
--- src/components/VueTableDynamic.vue
+++ src/components/VueTableDynamic.vue
@@ -54,9 +54,23 @@
               class="table-cell flex-c-s" 
               :class="{ 'border': tableBorder, 'is-header': (j === 0 && headerInfirstColumn ) }"
               :style="getCellStyle(j)"
-              @click="onClickCell(tableCell, j)"
+              @click="onClickCell(tableCell, i, j)"
             >
               <span class="table-cell-content">{{ tableCell.data }}</span>
+              <span v-if="i === 0 && headerInfirstRow && sortConfig.includes(j)" class="table-sort  flex-dir-column">
+                <i 
+                  class="sort-btns sort-ascending" 
+                  :class="{ 'activated': activatedSort[j] && activatedSort[j] === 'ascending' }"
+                  @click.stop="onSort(j, 'ascending')"
+                >
+                </i>
+                <i 
+                  class="sort-btns sort-descending" 
+                  :class="{ 'activated': activatedSort[j] && activatedSort[j] === 'descending' }"
+                  @click.stop="onSort(j, 'descending')"
+                >
+                </i>
+              </span>
             </div>
           </div>
         </div>
@@ -74,25 +88,29 @@ import '../assets/css/flex.css'
 import '../assets/iconfont/iconfont.css'
 
 export default {
-  name: 'DataTable',
+  name: 'VueTableDynamic',
   data () {
     return {
       tableData: {},
-      searchValue: ''
+      searchValue: '',
+      activatedSort: {}
     }
   },
   props: {
     // 表格相关信息
     // params.data: （二维矩阵）表格数据
-    // params.border: (布尔) 是否带边框。默认不带
-    // params.header: (字符串) 表头类型。 'row': 第一行作为表头； 'column': 第一列作为表头； ''/'none'/other: 无表头。 默认无
-    // params.showCheck: (布尔) 是否在第一列前显示多选（勾选）框。 默认不显示。  注：仅当params.header为'row时，第一行第一列为'全选'框，否则第一列均为当前行的勾选框
-    // params.enableUpload: (布尔) 启用上传功能。默认禁用
-    // params.enableDownload: (布尔) 启用下载功能。默认禁用
-    // params.enableSearch: (布尔) 启用搜索功能。默认禁用
+    // params.border: (Boolean) 是否带边框。默认不带
+    // params.header: (String) 表头类型。 'row': 第一行作为表头； 'column': 第一列作为表头； ''/'none'/other: 无表头。 默认无
+    // params.showCheck: (Boolean) 是否在第一列前显示多选（勾选）框。 默认不显示。  注：仅当params.header为'row时，第一行第一列为'全选'框，否则第一列均为当前行的勾选框
+    // params.enableUpload: (Boolean) 启用上传功能。默认禁用
+    // params.onUpload: (Function) 点击上传时的回调
+    // params.enableDownload: (Boolean) 启用下载功能。默认禁用
+    // params.onDownload: (Function) 点击下载时的回调
+    // params.enableSearch: (Boolean) 启用搜索功能。默认禁用
     // params.minWidth: (Number) table最小宽度。默认300
     // params.maxWidth: (Number) table最大宽度。默认1000
-    // params.columnWidth: (Array) 指定某一列或某几列的宽度，剩余列宽度均分. [{column: 0, width: 80}]
+    // params.columnWidth: (Array) 指定某一列或某几列的宽度，剩余列宽度均分. [{column: 0, width: 80}, {column: 1, width: '20%'}]
+    // params.sort: (Array) 指定以某列为基准排序。如指定第1列和第二列可排序：[0, 1]。 只在配置了第一行作为表头时有效
     params: { type: Object, default: () => { return {} } }
   },
   computed: {
@@ -117,8 +135,20 @@ export default {
     enableUpload () {
       return !!(this.params && this.params.enableUpload)
     },
+    uploadHandler () {
+      if (this.params && typeof this.params.onUpload === 'function') {
+        return this.params.onUpload
+      }
+      return () => {}
+    },
     enableDownload () {
       return !!(this.params && this.params.enableDownload)
+    },
+    downloadHandler () {
+      if (this.params && typeof this.params.onDownload === 'function') {
+        return this.params.onDownload
+      }
+      return () => {}
     },
     enableSearch () {
       return !!(this.params && this.params.enableSearch)
@@ -142,19 +172,30 @@ export default {
       if (this.params && unemptyArray(this.params.columnWidth)) {
         let obj = {}
         this.params.columnWidth.forEach(c => {
-          if (c && typeof c.column === 'number' && typeof c.width === 'number' && c.column >= 0) {
-            obj[c.column] = c.width
+          if (c && typeof c.column === 'number' && c.column >= 0) {
+            if (typeof c.width === 'number' && c.width >= 0) {
+              obj[c.column] = c.width + 'px'
+            } else if (typeof c.width === 'string' && /^(\d+\.?\d+?)%$/.test(c.width)) {
+              obj[c.column] = c.width
+            }
           }
         })
         return obj
       }
       return {}
+    },
+    sortConfig () {
+      if (this.params && this.params.header === 'row' && Array.isArray(this.params.sort) && this.params.sort.length > 0) {
+        return this.params.sort
+      }
+      return []
     }
   },
   watch: {
     params: {
       handler (value) {
         this.searchValue = ''
+        this.activatedSort = {}
       },
       deep: true,
       immediate: true
@@ -182,8 +223,6 @@ export default {
       }
     }
   },
-  mounted () {
-  },
   beforeDestroy () {
     this.tableData = {}
   },
@@ -192,7 +231,7 @@ export default {
       if (this.params && is2DMatrix(this.sourceData)) {
         let table = { key: unique(`table-`), checked: false, rows: [] }
         for (let i = 0; i < this.sourceData.length; i++) {
-          let tableRow = { key: unique(`table-`), checked: false, show: true }
+          let tableRow = { key: unique(`table-`), checked: false, show: true, index: i }
           tableRow.cells = this.sourceData[i].map(item => {
             return { data: item, key: unique(`table-`), checked: false }
           })
@@ -202,11 +241,11 @@ export default {
       }
     },
     getCellStyle (columnIndex) {
-      if (this.columnWidth[columnIndex] > 0) {
+      if (this.columnWidth[columnIndex]) {
         return {
           flexGrow: 0,
           flexShrink: 0,
-          flexBasis: this.columnWidth[columnIndex] + 'px'
+          flexBasis: this.columnWidth[columnIndex]
         }
       } else {
         return {
@@ -221,15 +260,18 @@ export default {
       let allChecked = (tableRow.checked !== true)
       this.setAllRowChecked(allChecked)
       this.$emit('select-all', allChecked)
-      this.$emit('selection-change', this.sourceData)
+      this.$emit(
+        'selection-change', 
+        this.getCheckedRowDatas(true), 
+        this.getCheckedRowIndexs(true),
+        this.getCheckedRowNum(true)
+      )
     },
     onCheckRow (tableRow, rowIndex) {
       if (!this.showCheck) return
 
       tableRow.checked = !tableRow.checked
-      this.$emit('select', rowIndex, tableRow, this.tableData)
-      this.$emit('selection-change', this.tableData)
-
+      
       if (this.headerInfirstRow) {
         if (this.isAllRowChecked()) {
           this.tableData.rows[0].checked = true
@@ -239,18 +281,58 @@ export default {
           this.tableData.rows[0].checked = false
         }
       }
+
+      this.$emit('select', tableRow.checked, rowIndex, this.sourceData[rowIndex])
+      this.$emit(
+        'selection-change', 
+        this.getCheckedRowDatas(true), 
+        this.getCheckedRowIndexs(true),
+        this.getCheckedRowNum(true)
+      )
     },
-    onClickRow (tableRow, index) {
-      this.$emit('row-click', index, tableRow)
+    onClickRow (tableRow, rowIndex) {
+      this.$emit('row-click', rowIndex, this.sourceData[rowIndex])
     },
-    onClickCell (tableCell, index) {
-      this.$emit('cell-click', index, tableCell)
+    onClickCell (tableCell, rowIndex, cellIndex) {
+      this.$emit('cell-click', rowIndex, cellIndex, this.sourceData[rowIndex][cellIndex])
     },
     onUpload () {
-      console.log('onUpload')
+      if (this.enableUpload) {
+        let datas = this.sourceData
+        if (this.showCheck) datas = this.getCheckedRowDatas(true)
+        this.$emit('upload', datas)
+        this.uploadHandler(datas)
+      }
     },
     onDownload () {
-      console.log('onDownload')
+      if (this.enableDownload) {
+        let datas = this.sourceData
+        if (this.showCheck) datas = this.getCheckedRowDatas(true)
+        this.$emit('download', datas)
+        this.downloadHandler(datas)
+      }
+    },
+    onSort (index, value) {
+      if (!(this.tableData && this.tableData.rows && this.tableData.rows.length > 0)) return
+      if (!this.headerInfirstRow) return
+      if (this.activatedSort[index] === value) return
+
+      this.activatedSort = {}
+      this.activatedSort[index] = value
+      
+      this.tableData.rows.sort((row1, row2) => {
+        if (row1.index === 0) return -1
+        if (row2.index === 0) return 1
+        let data1 = row1.cells[index].data
+        let data2 = row2.cells[index].data
+        if (value === 'ascending') {
+          return data1 > data2 ? 1 : -1
+        } else {
+          return data2 > data1 ? 1 : -1
+        }
+      })
+
+      this.$emit('sort-change', index, value)
     },
     filter () {
       if (!(this.enableSearch && this.tableData && this.tableData.rows)) return
@@ -272,14 +354,17 @@ export default {
         row ? row.show = true : ''
       })
     },
-    // 选中的行数
-    getCheckedRowNum () {
+    // 选中的行数。 includeWhenHeaderInfirstRow: 是否检查第一行表头
+    getCheckedRowNum (includeWhenHeaderInfirstRow = false) {
       if (!this.showCheck) return 0
 
       if (this.tableData && unemptyArray(this.tableData.rows)) {
         let num = 0
         this.tableData.rows.forEach((row, index) => {
-          if (index === 0 && this.headerInfirstRow) return
+          if (index === 0 && this.headerInfirstRow) {
+            if (!includeWhenHeaderInfirstRow) return
+            if (row.checked !== false) return (num++)
+          }
           if (row.checked) num++
         })
         return num
@@ -287,7 +372,7 @@ export default {
 
       return 0
     },
-    // 获取选中的行（数据） [{ row }]
+    // 获取选中的行 [{ row }]
     getCheckedRows () {
       if (!this.showCheck) return []
 
@@ -300,20 +385,32 @@ export default {
 
       return []
     },
-    // 获取选中的行（索引）
-    getCheckedRowIndexs () {
+    // 获取选中的行（索引）。includeWhenHeaderInfirstRow: 是否检查第一行表头
+    getCheckedRowIndexs (includeWhenHeaderInfirstRow = false) {
       if (!this.showCheck) return []
 
       if (this.tableData && unemptyArray(this.tableData.rows)) {
         let indexs = []
         this.tableData.rows.forEach((row, index) => {
-          if (index === 0 && this.headerInfirstRow) return
-          if (row.checked) indexs.push(index)
+          if (index === 0 && this.headerInfirstRow) {
+            if (!includeWhenHeaderInfirstRow) return
+            if (row.checked !== false) return indexs.push(row.index)
+          }
+          if (row.checked) indexs.push(row.index)
         })
         return indexs
       }
 
       return []
+    },
+    // 获取选中行的源数据（2DMatrix）。 includeWhenHeaderInfirstRow: 是否检查第一行表头
+    getCheckedRowDatas (includeWhenHeaderInfirstRow = false) {
+      if (!is2DMatrix(this.sourceData)) return []
+      let indexs = this.getCheckedRowIndexs(includeWhenHeaderInfirstRow)
+      let checkedDatas = this.sourceData.filter((s, i) => {
+        return indexs.includes(i)
+      })
+      return checkedDatas || []
     },
     // 是否所有行均为选中
     isAllRowChecked () {
@@ -343,6 +440,7 @@ export default {
 
 <style lang="scss" scoped>
 $borderColor: #DCDFE6;
+$rowHeight: 30px;
 
 .data-table{
   width: 100%;
@@ -362,7 +460,7 @@ $borderColor: #DCDFE6;
 }
 
 .table-row{
-  height: 30px;
+  height: $rowHeight;
   background-color: transparent;
   border-bottom: 1px solid $borderColor;
 }
@@ -455,6 +553,36 @@ $borderColor: #DCDFE6;
 .tools-search{
   width: 280px;
   margin-right: 8px;
+}
+
+.table-sort{
+  width: 20px;
+  height: $rowHeight;
+  margin-left: 2px;
+  position: relative;
+  vertical-align: middle;
+  .sort-btns{
+    width: 0;
+    height: 0;
+    border: 5px solid transparent;
+    position: absolute;
+    left: 5px;
+    cursor: pointer;
+  }
+  .sort-ascending{
+    border-bottom-color: #C0C4CC;
+    top: 4px;
+  }
+  .sort-descending{
+    border-top-color: #C0C4CC;
+    bottom: 4px;
+  }
+  .sort-ascending.activated{
+    border-bottom-color: #409EFF;
+  }
+  .sort-descending.activated{
+    border-top-color: #409EFF;
+  }
 }
 
 </style>
